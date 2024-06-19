f81cec4630a2c19e20a7cc0a6a534c0baee7ef7b
dakshatrawat <119694533+dakshatrawat@users.noreply.github.com>
2024-06-19 05:56:27 +0530

    Add files via upload

---
+++ q1.Untangle_String/test_case_generator.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string generateRandomString(int n, vector<char> chars)
+{
+    random_shuffle(chars.begin(), chars.end());
+    return string(chars.begin(), chars.begin() + n);
+}
+
+int main()
+{
+    srand(time(0));
+    int t;
+    cin >> t; // number of test cases needed
+    cout << t << endl;
+
+    for (int testCase = 0; testCase < t; testCase++)
+    {
+        int n = rand() % 10 + 1;
+        cout << n << endl;
+
+        vector<char> chars;
+        for (char c = 'a'; c < 'a' + n; c++)
+        {
+            chars.push_back(c);
+        }
+
+        string s = generateRandomString(n, chars);
+        string r = generateRandomString(n, chars);
+
+        cout << s << endl;
+        cout << r << endl;
+    }
+
+    return 0;
+}
